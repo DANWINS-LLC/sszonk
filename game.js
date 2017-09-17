c8ac219534ef4fad4e35feded989efdd48e0f777
sktzofrenic <dan@danwins.com>
2017-09-16 21:38:52 -0400

    Added audio

--- game.js
+++ game.js
@@ -15,21 +15,23 @@ var app = new Vue({
         cardPossibilities: [0, 0, 0, 0, 100, 200, 300, 400, 500]
     },
     methods: {
-        generateCards: function () {
+        generateCards: function() {
             this.changeTeam()
             this.cards = []
             for (var i = 0; i < _.range(24).length; i++) {
                 this.cards.push({
                     id: i,
                     flipped: false,
-                    value: this.cardPossibilities[Math.floor(Math.random()*this.cardPossibilities.length)]
+                    value: this.cardPossibilities[Math.floor(Math.random() * this.cardPossibilities.length)]
                 })
             }
         },
-        flip: function (index) {
+        flip: function(index) {
             this.cards[index].flipped = !this.cards[index].flipped
             if (this.cards[index].value === 0) {
                 // ZONK!!!
+                var audio = new Audio('audio/evillaugh.mp3')
+                audio.play()
                 if (this.risky) {
                     // Subtract points and end turn
                     this[this.currentTeam] -= this.activeTeamPoints
@@ -41,24 +43,34 @@ var app = new Vue({
             } else {
                 // Card was worth points
                 this.activeTeamPoints += this.cards[index].value * this.pickNumber
+                if (this.pickNumber === 1) {
+                    var audio = new Audio('audio/1_cheer.mp3')
+                    audio.play()
+                } else if (this.pickNumber === 2) {
+                    var audio = new Audio('audio/2_cheer.mp3')
+                    audio.play()
+                } else {
+                    var audio = new Audio('audio/3_cheer.mp3')
+                    audio.play()
+                }
                 this.pickNumber += 1
             }
         },
-        reveal: function () {
+        reveal: function() {
             var vm = this
             vm.risky = true
-            vm.cards.map(function (value) {
-                _.delay(function () {
+            vm.cards.map(function(value) {
+                _.delay(function() {
                     value.flipped = !value.flipped
-                }, Math.floor(Math.random() * 500) + 0)
+                }, Math.floor(Math.random() * 250) + 50)
             })
-            vm.cards.map(function (value) {
-                _.delay(function () {
+            vm.cards.map(function(value) {
+                _.delay(function() {
                     value.flipped = !value.flipped
-                }, Math.floor(Math.random() * 1000) + 500)
+                }, Math.floor(Math.random() * 600) + 500)
             })
         },
-        restart: function () {
+        restart: function() {
             this.teamOneName = ''
             this.teamOnePoints = 0
             this.teamTwoName = ''
@@ -68,7 +80,7 @@ var app = new Vue({
             this.risky = false
             this.pickNumber = 1
         },
-        endTurn: function () {
+        endTurn: function() {
             this[this.currentTeam] += this.activeTeamPoints
             this.activeTeamPoints = 0
             this.currentTeam = ''
@@ -76,15 +88,31 @@ var app = new Vue({
             this.pickNumber = 1
             this.generateCards()
         },
-        changeTeam: function (num) {
+        changeTeam: function(num) {
             this.currentTeam = num
             this.risky = false
             this.pickNumber = 1
             this.activeTeamPoints = 0
         }
     },
-    mounted () {
+    mounted() {
         this.generateCards()
+        var vm = this
+        Mousetrap.bind('e e', function() {
+            vm.endTurn()
+        })
+        Mousetrap.bind('left', function() {
+            vm.changeTeam('teamOnePoints')
+        })
+        Mousetrap.bind('right', function() {
+            vm.changeTeam('teamTwoPoints')
+        })
+        Mousetrap.bind('r r', function() {
+            vm.reveal()
+        })
+        Mousetrap.bind('s s', function() {
+            vm.generateCards()
+        })
     }
 
 })
